e45c0a4b4f2500b47ad5b7df201085209df60ca7
Alex Leger <chris.legerjr@gmail.com>
2019-11-26 15:11:21 -0500

    Added timeline beta and utility function

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import './index.css';
 import * as serviceWorker from './serviceWorker';
 import InOutChart from './InOutChart'
 import WeaponTypeChart from './WeaponTypeChart'
+import Timeline from './Timeline'
 
 //ReactDOM.render(<App />, document.getElementById('root'));
 
@@ -14,6 +15,7 @@ ReactDOM.render(<InOutChart />, document.getElementById('inoutchart'));
 
 ReactDOM.render(<WeaponTypeChart />, document.getElementById('weaponchart'));
 
+ReactDOM.render(<Timeline />, document.getElementById('timeline'))
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
