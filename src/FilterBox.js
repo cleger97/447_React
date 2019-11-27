e0d0f4b0630d6214d94d892c992e72726274217a
root <app@docker>
2019-11-27 23:16:37 +0000

    Filter box start

---
+++ src/FilterBox.js
@@ -0,0 +1,21 @@
+import React, {Component} from 'react';
+
+var crimeColumnVals = ["weapon",  "crimecode", "description"]
+var locationColumnVals = ["neighborhood", "post", "district", "premise", "location"]
+var locationFetch = "http://ec2-34-228-208-5.compute-1.amazonaws.com/crime-column-value/";
+var locationFetch = "http://ec2-34-228-208-5.compute-1.amazonaws.com/location-column-value/";
+
+
+export default class FilterBox extends Component {
+    
+
+    render() {
+	return (
+      <React.Fragment>
+        <h5> Crimes by Location </h5>
+        <div class = 'fill'>
+	<h1>Hi from peter {this.props.name}</h1>
+        </div>
+      </React.Fragment>)
+    }
+}
--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import InOutChart from './InOutChart'
 import WeaponTypeChart from './WeaponTypeChart'
 import HeatMap from './heatmap';
 import Timeline from './Timeline';
-
+import FilterBox from './FilterBox'
 
 //ReactDOM.render(<App />, document.getElementById('root'));
 
@@ -20,7 +20,9 @@ ReactDOM.render(<WeaponTypeChart />, document.getElementById('weaponchart'));
 
 ReactDOM.render(<HeatMap />, document.getElementById('heatmap'));
 
-ReactDOM.render(<Timeline />, document.getElementById('timeline'))
+ReactDOM.render(<Timeline />, document.getElementById('timeline'));
+
+ReactDOM.render(<FilterBox />, document.getElementById('dataForm'));
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
