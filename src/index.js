a87f7f514ff7baa531b743ce0960e5eae493ff2b
root <app@docker>
2019-11-29 17:58:57 +0000

    Manager component start

--- src/index.js
+++ src/index.js
@@ -7,11 +7,13 @@ import InOutChart from './InOutChart'
 import WeaponTypeChart from './WeaponTypeChart'
 import HeatMap from './heatmap';
 import Timeline from './Timeline';
-import FilterBox from './FilterBox'
+import FilterBox from './FilterBox';
+import FilterManager from './FilterManager';
 
 //ReactDOM.render(<App />, document.getElementById('root'));
 
 //ReactDOM.render(<DataHandle />, document.getElementById('data'));
+//ReactDOM.render(<FilterManager />, document.getElementById('manager'));
 
 ReactDOM.render(<InOutChart />, document.getElementById('inoutchart'));
 
@@ -23,6 +25,9 @@ ReactDOM.render(<HeatMap />, document.getElementById('heatmap'));
 ReactDOM.render(<Timeline />, document.getElementById('timeline'));
 
 ReactDOM.render(<FilterBox />, document.getElementById('dataForm'));
+
+
+
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
