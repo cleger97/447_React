b8f7cfaef9f461804b6d90749256a5c01912c941
Alex Leger <chris.legerjr@gmail.com>
2019-12-04 13:58:00 -0500

    Updated timeline to respond to filters

--- src/Timeline.js
+++ src/Timeline.js
@@ -5,6 +5,7 @@ import {Bar} from 'react-chartjs-2';
 
 import {genRandColor} from './utility.js';
 
+
 var simpleFetch = 'http://ec2-34-228-208-5.compute-1.amazonaws.com/date-count/?crimedate_gte=2014-01-01';
 
 export default class Timeline extends Component {
@@ -13,12 +14,30 @@ export default class Timeline extends Component {
     this.state = {
       keys: [],
       data: [],
-      colors: []
+      colors: [],
+      ready: false
     };
-    getSimpleFetch(this);
+  }
+
+  componentDidMount() {
+    if (this.props.filter != null) {
+      getSimpleFetch(this, this.props.filter);
+    } else {
+      getSimpleFetch(this, null);
+    }
+  }
+
+  componentDidUpdate(oldProps) {
+    if (this.props.filter != oldProps.filter) {
+      this.setState({ data: [], keys: [], colors: [], ready: false });
+      Promise.all([getSimpleFetch(this, this.props.filter)]);
+    }
   }
 
   render () {
+    if (this.state.ready == false) {
+      return null;
+    }
     console.log(this.state.keys);
     console.log(this.state.data);
 
@@ -34,7 +53,7 @@ export default class Timeline extends Component {
     }
 
 
-    return (
+    return (    
       <React.Fragment>
         <h5> Timeline </h5>
         <div class = 'fill'>
@@ -52,12 +71,24 @@ export default class Timeline extends Component {
 
 
 
-function getSimpleFetch(obj) {
+function getSimpleFetch(obj, filter) {
   var retKeys = [];
   var retData = [];
   var colorList = [];
 
-  fetch(simpleFetch, {
+  var yearSet = [];
+
+  const monthSet = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+
+  var date = new Date();
+
+  for (var i = 2014; i <= date.getFullYear(); i++) {
+    yearSet.push(i);
+  }
+
+  filter = filter.replace("?", "&");
+
+  fetch(simpleFetch + filter, {
     headers : {
       'Content-Type': 'application/json', 
       'Accept': 'application/json',
@@ -65,21 +96,38 @@ function getSimpleFetch(obj) {
   }).then((res) => {
     return res.json();
   }).then(data => {
-    console.log(data);
-    console.log(Object.keys(data));
-    Object.keys(data).forEach((key, value) => {
+    //console.log(data);
+    //console.log(Object.keys(data));
+
+    yearSet.forEach((key) => {
       console.log(key);
-      Object.keys(data[key]).forEach((key2, value2) => {
+      if (data[key] == null) { 
+        for (var i = 1; i < 13; i++) {
+          retKeys.push(key + " " + i);
+          retData.push(0);
+          colorList.push(genRandColor(0.8));
+        }
+        return;
+      }
+      monthSet.forEach((key2, value2) => {
+
         console.log(key2 + " " + data[key][key2]);
         var inKey = key + " " + key2;
         var inData = data[key][key2];
+        
         retKeys.push(inKey);
-        retData.push(inData);
-        colorList.push(genRandColor());
+        
+        if (inData == null) {
+          retData.push(0);
+        } else {
+          retData.push(inData);
+        }
+
+        colorList.push(genRandColor(0.8));
       });
     });
 
-    obj.setState({ keys: retKeys, data: retData, colors: colorList})
+    obj.setState({ keys: retKeys, data: retData, colors: colorList, ready: true})
   });
 
   return null;
--- src/WeaponTypeChart.js
+++ src/WeaponTypeChart.js
@@ -1,76 +1,76 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import ChartJS from 'chart.js';
-import {Doughnut} from 'react-chartjs-2';
+import { Doughnut } from 'react-chartjs-2';
 
 var weaponFetch = "http://ec2-34-228-208-5.compute-1.amazonaws.com/column-count/?column=weapon";
 
 export default class WeaponTypeChart extends Component {
-    constructor(props) {
-	super(props);
-	
-	this.state = {
-	    keys: [],
-	    data: [],
-	    ready: false
-	};
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      keys: [],
+      data: [],
+      ready: false
+    };
+  }
+
+  componentDidMount() {
+    Promise.all([loadWeapons(this, this.props.filter)]);
+  }
+
+  componentDidUpdate(oldProps) {
+    if (this.props.filter != oldProps.filter) {
+      this.setState({ data: {}, keys: {}, ready: false });
+      Promise.all([loadWeapons(this, this.props.filter)]);
     }
-    
-    componentDidMount() {
-	Promise.all([loadWeapons(this, this.props.filter)]);
+  }
+
+  render() {
+
+    if (this.state.ready == false) {
+      return null;
     }
-    
-    componentDidUpdate(oldProps){
-	if(this.props.filter != oldProps.filter){
-	    this.setState({data: {}, keys: {}, ready: false});
-	    Promise.all([loadWeapons(this, this.props.filter)]);
-      }
+    var dataValues = this.state.data;
+    var data = {
+      labels: this.state.keys,
+      datasets: [{
+        data: this.state.data,
+      }]
     }
-    
-    render() {
-	
-	if(this.state.ready == false){
-	    return null;
-	}
-	var dataValues = this.state.data;
-	var data = {
-            labels: this.state.keys,
-            datasets: [{
-		data: this.state.data,
-            }]
-	} 
-	var options = {
-	    maintainAspectRatio : false
-	}
-    
-	return (
-		<React.Fragment>
-		<div class = 'fill'>
-		<Doughnut data = {data} options = {options} redraw />
-		</div>
-		</React.Fragment>
-	);
+    var options = {
+      maintainAspectRatio: false
     }
+
+    return (
+      <React.Fragment>
+        <div class='fill'>
+          <Doughnut data={data} options={options} redraw />
+        </div>
+      </React.Fragment>
+    );
+  }
 }
 
 
 function loadWeapons(obj, filter) {
-    filter = filter.replace("?", "&");
-    fetch(weaponFetch + filter, {
-      headers : { 
-        'Content-Type': 'application/json',
-        'Accept': 'application/json',
-       }
- 
-    })
+  filter = filter.replace("?", "&");
+  fetch(weaponFetch + filter, {
+    headers: {
+      'Content-Type': 'application/json',
+      'Accept': 'application/json',
+    }
+
+  })
     .then((res) => res.json())
     .then(data => {
       var keyVal = Object.keys(data);
       var values = Object.values(data);
 
       obj.setState({
-        keys : keyVal,
-          data: values,
-	  ready: true
+        keys: keyVal,
+        data: values,
+        ready: true
       });
-    }); 
+    });
 }
