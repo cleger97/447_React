70a66597657f8aaa0d7628787bc01198a42bac0e
root <app@docker>
2019-12-13 01:40:29 +0000

    Move clear filter button to below the table

--- src/Table.js
+++ src/Table.js
@@ -2,14 +2,14 @@ import React, {Component} from 'react';
 
 var crimeFetch = "http://ec2-34-228-208-5.compute-1.amazonaws.com/crimeinstances/";
 
-var columns = ["crimecode", "crimedate", "crimetime", "description", "district", "inside_outside", "latitude", "longitude", "location", "neighborhood", "post", "premise", "weapon"]
-var column_headers = ["Code", "Date", "Time", "Description", "District", "Inside Outside", "Latitude", "Longitude", "Location", "Neighborhood", "Post", "Premise", "Weapon"]
+var columns = ["crimecode", "crimedate", "crimetime", "description", "district", "inside_outside", "latitude", "longitude", "location", "neighborhood", "postp", "premise", "weapon"]
+var column_headers = ["#", "Code", "Date", "Time", "Description", "District", "Inside Outside", "Latitude", "Longitude", "Location", "Neighborhood", "Post", "Premise", "Weapon"]
 
 export default class Table extends Component {
 
     constructor(props){
 	super(props);
-	this.state = {filter: "", ready: false, data: {}, keys: {}};
+	this.state = {page: 1, filter: "", ready: false, data: {}, keys: {}, maxPage:0};
 	this.getNext = this.getNext.bind(this)
 	this.getPrev = this.getPrev.bind(this)
     }
@@ -61,13 +61,19 @@ export default class Table extends Component {
 	    </tr>
 	    </thead>
 
+	if(this.state.page == 1){
+	    var start = 1;
+	}else{
+	    var start = (100 * (this.state.page - 1)) + 1;
+	}
+	var curr_id = start;
 	//build the rows
 	var rows=[];
 	var results = this.state.data.results;
 	for(var i = 0; i < results.length; i++){
 	    var column_vals = [];
-
-
+	    
+	    column_vals.push(<td>{curr_id}</td>);
 	    //build the columns
 	    for(var j = 0; j < columns.length; j++){
 		var curr_val = results[i][columns[j]];
@@ -79,13 +85,17 @@ export default class Table extends Component {
 		</tr>
 		
 	    rows.push(row);
+	    curr_id++;
 	}
 	
 	return (
 		<React.Fragment>
 		<div class="col-md-12 table-outer">
+		<div style={{"display": "flex"}}>
 		<button onClick={this.getPrev}>Prev</button>
 		<button onClick={this.getNext}>Next</button>
+		<h5>{this.state.page}/{this.state.maxPage}</h5>
+		</div>
 		<div class = "table-holder" style={{"padding": "15px"}}>
 
 		
@@ -118,12 +128,22 @@ function getData(obj, filter, page){
 	    'Accept': 'application/json',
 	}
 	
-    })
-	.then((res) => res.json())
+    }).then((res) => res.json())
 	.then(data => {
+	    var maxPage = Math.ceil(data.count/100);
+	    if(url.includes("page=")){
+		var pre_page = url.split("page=")[1]
+		var end = pre_page.indexOf("&")
+		if(end == -1 || end == null)
+		    end = pre_page.length
+		var page = parseInt(pre_page.substring(0, end));
+	    }else{
+		var page = 1;
+	    }
 	    obj.setState({
-	    
+		page: page,
 		data: data,
+		maxPage: maxPage,
 		ready: true
 	    });
 	});
